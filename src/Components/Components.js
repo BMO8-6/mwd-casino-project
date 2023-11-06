84444743acd20880ef2929aa1c005b7e098afe52
jgutier7 <jgutier7@nd.edu>
2023-11-06 18:58:41 -0500

    Feature 5: added base uthorization

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,16 +1,30 @@
+import React from "react";
 import HomeApp from "./Home/HomeApp.js";
 import Blackjack from "./Blackjack/BlackjackApp.js";
 import Roulette from "./Roulette/RouletteApp.js";
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.js"; 
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 const Components = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomeApp />} />
-        <Route path="/blackjack" element={<Blackjack />} />
-        <Route path="/roulette" element={<Roulette />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/auth/register" element={<AuthRegister />} />
+        <Route path="/auth/login" element={<AuthLogin />} />
+        <Route path="/" element={
+          <ProtectedRoute element={HomeApp} />
+        } />
+        <Route path="/blackjack" element={
+          <ProtectedRoute element={Blackjack} />
+        } />
+        <Route path="/roulette" element={
+          <ProtectedRoute element={Roulette} />
+        } />
+        <Route path="/auth" element={<AuthModule />} /> 
+        <Route path="*" element={<Navigate to="/auth" replace />} />
       </Routes>
     </Router>
   );
